ba718943a55d1c71b37f83c117e1d3aa8b768473
Siddharth Santhosh <siddharthsanthosh@MacBook-Pro.local>
2025-07-08 02:31:28 -0400

    Text Change Check

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
-
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
